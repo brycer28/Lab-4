fbaea1e153fe5d73c7c90bdeba16002cf2a03178
brycer28 <brycer2825@gmail.com>
2024-11-20 15:21:12 -0600

    added 2 design patterns

--- src/AddEventModal.java
+++ src/AddEventModal.java
@@ -2,7 +2,6 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 
 public class AddEventModal extends JDialog {
--- src/EventListPanel.java
+++ src/EventListPanel.java
@@ -3,7 +3,6 @@ import javax.swing.border.EmptyBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.lang.reflect.Array;
 import java.time.*;
 import java.util.*;
 
